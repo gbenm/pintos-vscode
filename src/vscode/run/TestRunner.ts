9595328d11fa1c773eca5423c3a0620bb28c375e
Benyamin Galeano <benyamin.galeano@gmail.com>
2022-10-26 21:01:40 -0600

    feat: 🎸 compile before run

--- src/vscode/run/TestRunner.ts
+++ src/vscode/run/TestRunner.ts
@@ -1,8 +1,11 @@
 import * as vscode from "vscode"
 import { TestItem } from "../../core/grade/TestItem"
+import { childProcessToPromise } from "../../core/launch"
 import { TestController, TestLotProcess } from "../PintosTestController"
 
 export default class TestRunner extends TestLotProcess {
+  private compiledPhases: string[] = []
+
   constructor (args: {
     request: Partial<vscode.TestRunRequest>
     controller: TestController
@@ -11,10 +14,30 @@ export default class TestRunner extends TestLotProcess {
   }
 
   protected async execute(test: TestItem<vscode.TestItem>): Promise<void> {
+    await this.compileIfNeeded(test)
+
     await test.run({
       output: this.controller.output,
       runningTestid: test.gid,
       shell: this.shell
     })
+  }
+
+  private async compileIfNeeded (test: TestItem) {
+    if (!this.compiledPhases.includes(test.phase)) {
+      this.controller.output?.appendLine(`[make] compile ${test.name}\n`)
+      await childProcessToPromise({
+        process: this.shell.make({
+          cwd: test.phase,
+          args: []
+        }),
+        onData: (buffer: Buffer) => {
+          this.controller.output?.append(buffer.toString())
+        }
+      })
+      this.controller.output?.appendLine("")
+
+      this.compiledPhases.push(test.phase)
+    }
   }
 }
